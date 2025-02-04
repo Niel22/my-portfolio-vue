7c34309cb5e5057723b246a7871d12a4e0eecd3f
Niel22 <niel2264@gmail.com>
2025-02-04 18:36:01 +0100

    Updated

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="w-100 bgn1-color p-3 position-fixed z-3 top-0 d-lg-none d-flex align-items-center justify-content-between br-bottom-n5 box-shadow1"
-        id="top_header"><a href="/" class="side-icon p1-color bgn2-color brn4">
+        id="top_header"><router-link :to="{name: 'index'}" class="side-icon p1-color bgn2-color brn4">
             <i class="ph ph-seal-check"></i>
-        </a>
+        </router-link>
 
         <div class="d-flex gap-3 align-items-center">
             <button class="side-icon bg1-color mood_toggle">
