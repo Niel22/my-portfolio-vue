eb8e316c84ac88866fad9d53460a37ef316f8a6e
Niel22 <niel2264@gmail.com>
2025-02-04 18:27:22 +0100

    Start

--- src/components/BottomHeader.vue
+++ src/components/BottomHeader.vue
@@ -1,5 +1,43 @@
 <template>
-<div class="w-100 bgn1-color p-3 position-fixed z-3 bottom-0 d-block d-lg-none br-top-n5 box-shadow1" id="bottom_header"></div>
+    <div class="w-100 bgn1-color p-3 position-fixed z-3 bottom-0 d-block d-lg-none br-top-n5 box-shadow1"
+        id="bottom_header">
+        <div class="header-bottom-menu w-full">
+            <ul class="d-flex gap-1 align-items-center justify-content-between">
+                <li class="rounded-3">
+                    <router-link :to="{name: 'index'}" class="d-flex align-items-center gap-2 n5-color fs-eight p-2">
+                        <span class="fs-five d-flex align-items-center justify-content-center">
+                            <i class="ph-fill ph-user"></i>
+                        </span>
+                        <span class="d-none d-md-block">About Me</span></router-link>
+                </li>
+                <li class="bg1-color">
+                    <router-link :to="{name: 'resume'}" class="d-flex align-items-center gap-2 n5-color fs-eight p-2">
+                        <span class="fs-five d-flex align-items-center justify-content-center"><i
+                                class="ph-fill ph-notebook fs-six"></i></span>
+                        <span class="d-none d-md-block">Resume</span>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link :to="{name: 'portfolio'}" class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex align-items-center gap-2 n5-color fs-eight p-2">
+                            <span class="fs-five d-flex align-items-center justify-content-center">
+                                <i class="ph-fill ph-code-block"></i>
+                            </span>
+                            <span class="d-none d-md-block">Portfolio</span>
+                        </div>
+                        <span class="n5-color bg2-color fs-ten px-1 pt-1 rounded-2 me-3 d-none d-md-block">16</span>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link :to="{name: 'contact'}" class="d-flex align-items-center gap-2 n5-color fs-eight p-2">
+                        <span class="fs-five d-flex align-items-center justify-content-center"><i
+                                class="ph-fill ph-envelope"></i></span>
+                        <span class="d-none d-md-block">Contact</span>
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="w-100 bgn1-color p-3 position-fixed z-3 top-0 d-lg-none d-flex align-items-center justify-content-between br-bottom-n5 box-shadow1"
-        id="top_header">
+        id="top_header"><a href="/" class="side-icon p1-color bgn2-color brn4">
+            <i class="ph ph-seal-check"></i>
+        </a>
+
+        <div class="d-flex gap-3 align-items-center">
+            <button class="side-icon bg1-color mood_toggle">
+                <i class="mood_icon ph-fill fs-six n11-color ph-sun"></i>
+            </button>
+        </div>
     </div>
 </template>
 
